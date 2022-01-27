5a61d90af52aec886720237a1d4b66ecb2355689
Daniel Jung <wntls4464@gmail.com>
2022-01-27 22:36:44 +0900

    11 Commit

--- myservice/js/index.js
+++ myservice/js/index.js
@@ -17,4 +17,40 @@ $(document).ready(function() {
         signup.slideUp()
         introSite.slideDown()
     })
+
+    var genderWoman = $('#gMW')
+    var genderMan = $('#gMM')
+
+    genderWoman.click(function(){
+        genderBgInit()
+        $(this).css('background','#64cbf9')
+        $(this).css('color',"#fff")
+    })
+
+    genderMan.click(function(){
+        genderBgInit()
+        $(this).css('background','#64cbf9')
+        $(this).css('color','#fff')
+    })
+
+    function genderBgInit() {
+        genderMan.css('background','#fff')
+        genderWoman.css('background','#fff')
+        genderMan.css('color','#000')
+        genderWoman.css('color','#000')
+    }
+
+    toGoToShort = false
+    $(window).resize(function(){
+        if(window.innerWidth >= 1200){
+            loginForm.slideDown()
+            signup.slideDown()
+            introSite.slideDown()
+            toGoToShort = true
+        }else{
+            if(toGoToShort == true){
+                signup.slideUp()
+            }
+        }
+    })
 })
